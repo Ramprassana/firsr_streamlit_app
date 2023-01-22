e7f4106d8932c396caa231edae9dc3b457a8a206
Ramprassana <123292781+Ramprassana@users.noreply.github.com>
2023-01-22 15:06:25 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,10 +3,14 @@ import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
 
-streamlit.header('Breakfast Menu')
+streamlit.header('Breakfast Favorites')
 
 streamlit.text('Omega3 & Blueberry Oatmeal')
 
 streamlit.text('Kale, Spinach & Rocket Smoothie')
 
 streamlit.text('Hard-Boiled Free-Range Egg')
+
+streamlit.text('Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
